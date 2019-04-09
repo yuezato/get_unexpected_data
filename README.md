cf115d01eae03aea1f7315bc048c1c579b92d865
yuuya uezato <yuuya_uezato@dwango.co.jp>
2019-04-09 20:16:26 +0900

    issue再現用のリポジトリ作成

---
+++ README.md
@@ -0,0 +1,36 @@
+# CannyLSのissue?を再現する
+
+## 手順
+```bash
+$ cargo build --all
+
+$ rm -f test.lusf
+
+$ ./target/debug/get_unexpected_data --phase=1
+put `hoge` to lump_id1(= LumpId("00000000000000000000000000000457")).
+delete lump_id1(= LumpId("00000000000000000000000000000457")).
+put `foo` to lump_id2(= LumpId("0000000000000000000000000153158e")).
+
+$ ./target/debug/get_unexpected_data --phase=2
+try to read a datum from lump_id1(= LumpId("00000000000000000000000000000457")).
+We deleted lump_id1(= LumpId("00000000000000000000000000000457")); however, we can read a datum from lump_id1.
+Furthermore, the read data `foo` is not `hoge`.
+```
+
+## メッセージの説明
+1. `phase=1`では以下のことを行う:
+    a. `lump_id1`に"hoge"をputする。
+    b. この段階でjournalをdiskに同期する。
+    c. `lump_id1`をdeleteする。
+    d. `lump_id2`に"foo"をputする。
+    e. `mem::forget`を呼び出して、プロセスのクラッシュを模倣する。
+2. `phase=2`では以下のことを行う:
+    a. `lump_id1`をgetしようとする。
+    b. 削除した筈の`lump_id1`からデータが読み込める。
+    c. 読み込むデータは"hoge"ではなく"foo"である。
+
+## 原因
+* `lump_id1`をdeleteした段階では、deleteした情報はdiskに永続化されていない。
+* 一方で、メモリアロケータは"hoge"の位置にデータを書くことができる。
+* 結果的に、`lump_id2`に対するputで"hoge"のあった位置に"foo"を書き込んでしまう。
+* 再起動後には、`lump_id1`は生存しているように見えてしまい、結果として"foo"を読み出す。
---
+++ src/main.rs
@@ -0,0 +1,69 @@
+extern crate structopt;
+#[macro_use]
+extern crate trackable;
+
+use std::mem;
+use structopt::StructOpt;
+
+use cannyls::block::BlockSize;
+use cannyls::lump::*;
+use cannyls::nvm::FileNvm;
+use cannyls::storage::Storage;
+use cannyls::Error;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "get_unexpected_data")]
+struct Opt {
+    #[structopt(long = "phase")]
+    phase: usize,
+}
+
+fn main() -> Result<(), Error> {
+    let path = "test.lusf";
+    let opt = Opt::from_args();
+
+    let lump_id1 = LumpId::new(1_111);
+    let lump_id2 = LumpId::new(22_222_222);
+
+    if opt.phase == 1 {
+        let nvm = track!(FileNvm::create(
+            path,
+            BlockSize::min().ceil_align(1024 * 1024)
+        ))?;
+        let mut storage = track!(Storage::create(nvm))?;
+
+        let lump_data1 = track!(storage.allocate_lump_data_with_bytes(b"hoge"))?;
+
+        println!("put `hoge` to lump_id1(= {:?}).", lump_id1);
+        track!(storage.put(&lump_id1, &lump_data1))?;
+        track!(storage.journal_sync())?;
+
+        println!("delete lump_id1(= {:?}).", lump_id1);
+        track!(storage.delete(&lump_id1))?;
+
+        println!("put `foo` to lump_id2(= {:?}).", lump_id2);
+        let lump_data2 = track!(storage.allocate_lump_data_with_bytes(b"foo"))?;
+        track!(storage.put(&lump_id2, &lump_data2))?;
+
+        mem::forget(storage);
+        Ok(())
+    } else if opt.phase == 2 {
+        let nvm = track!(FileNvm::open(path))?;
+        let mut storage = track!(Storage::open(nvm))?;
+
+        println!("try to read a datum from lump_id1(= {:?}).", lump_id1);
+        let v = track!(storage.get(&lump_id1))?;
+        if let Some(v) = v {
+            println!("We deleted lump_id1(= {:?}); however, we can read a datum from lump_id1.", lump_id1);
+            println!(
+                "Furthermore, the read data `{}` is not `hoge`.",
+                String::from_utf8_lossy(v.as_bytes())
+            );
+        } else {
+            panic!("unexpected behaviour for v0.9.3");
+        }
+        Ok(())
+    } else {
+        panic!("--phase=1 or --phase=2 are only allowed");
+    }
+}
